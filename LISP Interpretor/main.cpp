52675002f44b6e669e6080e5c3b176c299a1ce92
RajadityaM <rajadityamukherjee.osu@outlook.com>
2013-02-22 17:50:46 -0500

    Initial Commit

---
+++ LISP Interpretor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LISP Interpretor
+//
+//  Created by Rajaditya Mukherjee on 2/22/13.
+//  Copyright (c) 2013 Rajaditya Mukherjee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
