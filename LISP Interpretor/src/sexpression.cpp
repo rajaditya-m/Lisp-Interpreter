6ea5b629a4fc6cc92a8a27ad56d92c0c3e701d90
RajadityaM <rajadityamukherjee.osu@outlook.com>
2013-02-22 19:11:24 -0500

    S Expression class

--- LISP Interpretor/src/sexpression.cpp
+++ LISP Interpretor/src/sexpression.cpp
@@ -7,3 +7,43 @@
 //
 
 #include "sexpression.h"
+
+SExp::SExp()
+{
+    car_= cdr_= NULL;
+    stringVal_ = NULL;
+    isAtom_ = false;
+    isString_ = false;
+    isNull_ = true;
+}
+
+SExp::SExp(int val)
+{
+    car_= cdr_= NULL;
+    stringVal_ = NULL;
+    intVal_ = val;
+    isAtom_ = true;
+    isString_ = false;
+    isNull_ = false;
+}
+
+SExp::SExp(char* str)
+{
+    car_= cdr_= NULL;
+    stringVal_ = str;
+    isAtom_ = true;
+    isString_ = true;
+    isNull_ = false;
+}
+
+SExp::SExp(SExp* cr,SExp* cd)
+{
+    car_= cr;
+    cdr_= cd;
+    stringVal_ = NULL;
+    isAtom_ = false;
+    isString_ = false;
+    isNull_ = false;
+}
+
+
