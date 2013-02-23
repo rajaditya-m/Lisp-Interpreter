616aec0efdd7618c8422e78d2fde25175c899a4f
RajadityaM <rajadityamukherjee.osu@outlook.com>
2013-02-22 19:11:49 -0500

    Headers for sexp

--- LISP Interpretor/src/sexpression.h
+++ LISP Interpretor/src/sexpression.h
@@ -1,21 +1,42 @@
 //
-//  satoms.h
+//  sexpression.h
 //  LISP Interpretor
 //
 //  Created by Rajaditya Mukherjee on 2/22/13.
 //  Copyright (c) 2013 Rajaditya Mukherjee. All rights reserved.
 //
 
-#ifndef __LISP_Interpretor__satoms__
-#define __LISP_Interpretor__satoms__
+#ifndef __LISP_Interpretor__sexpression__
+#define __LISP_Interpretor__sexpression__
 
 #include <iostream>
-public class    
-{
-public:
-    SExpression* getCAR() {return car;}
-    SExpression* getCDR() {return cdr;}
-    int 
-}
+#include <cstring>
 
-#endif /* defined(__LISP_Interpretor__satoms__) */
+class SExp
+{
+    
+public:
+    SExp* getCAR()          {return car_;           }
+    SExp* getCDR()          {return cdr_;           }
+    int getIntegerID()      {return intVal_;        }
+    char* getStringID()     {return stringVal_;     }
+    bool isAtom()           {return isAtom_;        }
+    bool isString()         {return isString_;      }
+    bool isNull()           {return isNull_;        }
+    //Functions that will be defined in headers
+    SExp();
+    SExp(int val);
+    SExp(char* str);
+    SExp(SExp* cr,SExp* cd);
+    
+private:
+    SExp* car_;
+    SExp* cdr_;
+    int intVal_;
+    char* stringVal_;
+    bool isAtom_;
+    bool isString_;
+    bool isNull_;
+};
+
+#endif /* defined(__LISP_Interpretor__sexpression__) */
